9637ee7f19e9b1f2346574be8d1fe9687df8c069
timotin <w.timon@gmail.com>
2015-01-09 01:40:17 +0100

    datasource read

--- src/App.java
+++ src/App.java
@@ -1,8 +1,12 @@
+import datasource.DataSourceReader;
+
 /**
  * Created by Alex on 09.01.2015.
  */
 public class App {
     public static void main(String[] args){
         System.out.println("Hello");
+        DataSourceReader dataSourceReader = new DataSourceReader();
+        dataSourceReader.start();
     }
 }
