8cec3a541860838ea3f680bac579850c96f151ed
sawa112 <AlexanderSawadski@gmail.com>
2015-01-10 15:07:48 +0100

    Station: Sender erstellt

--- src/Common.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Alex on 10.01.2015.
- */
-public class Common {
-}
--- src/station/Station.java
+++ src/station/Station.java
@@ -1,8 +1,65 @@
 package station;
 
-public class Station extends Thread{
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
 
-    public Station(){
+import station.datasource.DataSourceReader;
 
-    }
+public class Station extends Thread {
+
+	/** Time To Life in sekunden */
+	private final int TTL_IN_SEC = 1;
+	/** Wartezeit des Senders in millisekunden */
+	private final long SENDER_WAIT_TIME_IN_MILLISEC = 1000;
+
+	/** Netzwerkinterface Name */
+	private String interfaceName;
+	/** Multicast Adresse */
+	private String mcastAddress;
+	/** Port auf den gelauscht wird */
+	private int receivePort;
+	/** Stationklasse */
+	private String stationClass;
+
+	/** DatenSourceReader */
+	private DataSourceReader dataSourceReader;
+	/** Sender */
+	private Sender sender;
+	/** Multicastsocket */
+	private MulticastSocket multicastSocket;
+
+	public Station(String interfaceName, String mcastAddress, int receivePort,
+			String stationClass) {
+		this.interfaceName = interfaceName;
+		this.mcastAddress = mcastAddress;
+		this.receivePort = receivePort;
+		this.stationClass = stationClass;
+
+		// Create DataSourceReader
+		this.dataSourceReader = new DataSourceReader();
+
+		// Create Sender
+		try {
+			multicastSocket = new MulticastSocket(receivePort);
+			multicastSocket.setTimeToLive(TTL_IN_SEC);
+			multicastSocket.setNetworkInterface(NetworkInterface
+					.getByName(interfaceName));
+			multicastSocket.joinGroup(InetAddress.getByName(mcastAddress));
+			this.sender = new Sender(multicastSocket,
+					SENDER_WAIT_TIME_IN_MILLISEC, mcastAddress, receivePort);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Create Receiver
+
+		// TODO: Create Output (Datensenke)
+	}
+
+	@Override
+	public void run() {
+
+	}
 }
