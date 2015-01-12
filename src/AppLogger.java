0d4bb6631174588d800658bf622b7ad8766f95c7
timotin <w.timon@gmail.com>
2015-01-12 22:53:33 +0100

    logger

---
+++ src/AppLogger.java
@@ -0,0 +1,8 @@
+import station.Logger;
+
+public class AppLogger {
+	public static void main(String[] args) {
+		Logger log = new Logger();
+		log.printMessages(null);
+	}
+}
--- src/station/Logger.java
+++ src/station/Logger.java
@@ -1,15 +1,34 @@
 package station;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
 
 public class Logger {
 
+	private java.util.logging.Logger logger;
+	private FileHandler fh;
+
 	public Logger() {
+		logger = java.util.logging.Logger.getLogger("MyLog");
+		try {
+			fh = new FileHandler("MyLogFile.log");
+			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void printMessages(List<Message> allReceivedMessage) {
 		for (Message message : allReceivedMessage) {
-			System.out.println(message.toString());
+			logger.info(message.toString());
 		}
 	}
 
